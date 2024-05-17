24993c8706b4aa1c8ebc5730d3e615d06f98d379
10221022jacqui <montes.rodriguez.jacqueline.guadalupe.22@cetis156.edu.mx>
2024-05-17 14:47:56 -0700

    Update main.py

--- Opereciones/main.py
+++ Opereciones/main.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return render_template('index.html')
+    return render_template('base.html')
 
 @app.route('/resta', methods=['POST'])
 def resta():
